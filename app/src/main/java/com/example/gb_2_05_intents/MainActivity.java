de11af734e4109ec1f68e414916870e520251d79
Vitaliy <v.a.grahov@ya.ru>
2021-04-24 23:43:40 +0500

    Open SettingsActivity from MainActivity

--- app/src/main/java/com/example/gb_2_05_intents/MainActivity.java
+++ app/src/main/java/com/example/gb_2_05_intents/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gb_2_05_intents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +27,19 @@ public class MainActivity extends AppCompatActivity {
             String name = txtName.getText().toString();
             String sayHello = getString(R.string.say_hello) + name;
             txtGreetings.setText(sayHello);
+        });
+
+        Button btnSettings = findViewById(R.id.btnSettings);
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Чтобы стартовать активити, надо подготовить интент
+                // В данном случае это будет явный интент, поскольку здесь передаётся класс активити
+                Intent runSettings = new Intent(MainActivity.this, SettingsActivity.class);
+                // Метод стартует активити, указанную в интенте
+                startActivity(runSettings);
+
+            }
         });
     }
 }
